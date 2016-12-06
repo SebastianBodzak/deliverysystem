65ba96c531849073a2b21262a5a5b2c0d29ef91d
Sebastian Bodzak <sebastian.bodzak@impaqgroup.com>
2016-12-06 11:49:10 +0100

    Initial commit

---
+++ src/main/java/com/testgroup/DeliverysystemApplication.java
@@ -0,0 +1,12 @@
+package com.testgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverysystemApplication.class, args);
+	}
+}
